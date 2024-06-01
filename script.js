a3e71736e2a4d30c86c3f550e649263a83a0fd87
dinaamlll <dinamaliaa17@gmail.com>
2024-06-01 22:44:09 +0700

    mengambil nilai dari elemen select dengan id "favoriteGenre" (genre musik favorit) dan menyimpannya dalam variabel favoriteGenre.

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ function KirimData(event) {
     event.preventDefault();
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-}   var dob = document.getElementById("dob").value;
+    var dob = document.getElementById("dob").value;
+    var favoriteGenre = document.getElementById("favoriteGenre").value;
+
+}
