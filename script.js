6b97e6edf4a6d5f3c150be0e279d94a07ca86467
dinaamlll <dinamaliaa17@gmail.com>
2024-06-01 22:43:31 +0700

    mengambil nilai dari elemen input dengan id "dob" (tanggal lahir) dan menyimpannya dalam variabel dob.

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ function KirimData(event) {
     event.preventDefault();
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-}
+}   var dob = document.getElementById("dob").value;
