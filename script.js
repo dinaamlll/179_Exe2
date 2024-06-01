475e5ee4f2b1f158d1a1bfe7c8ba2ac2729d89bf
dinaamlll <dinamaliaa17@gmail.com>
2024-06-01 22:44:37 +0700

    untuk mendapatkan nilai opini yang dipilih oleh pengguna dan menyimpannya dalam variabel opinion.

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ function KirimData(event) {
     var email = document.getElementById("email").value;
     var dob = document.getElementById("dob").value;
     var favoriteGenre = document.getElementById("favoriteGenre").value;
-
+    var opinion = document.querySelector('input[name="opinion"]:checked').value;
+    
 }
